4b907852ed52fc26ad0fd9cd2193a5408c763aa5
Amir Khan <ak2022dev@gmail.com>
2022-12-31 16:20:07 +0000

    TDD add method to raise error if existing user tries to sign-up

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -4,12 +4,20 @@ class Chitter
     @@users = {}
   end
 
-  def self.sign_up(email, password)
-    @@users[email] = password
-  end
-
   def self.get_users
     return @@users
+  end
+
+  def self.sign_up(email, password)
+    unless self.user_exists(email)
+      @@users[email] = password
+    else
+      raise("User already exists")
+    end
+  end
+
+  def self.user_exists(email)
+    return @@users.include?(email)
   end
 
 end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe "Chitter" do
       Chitter.sign_up("test@email.com", "password")
       expect(Chitter.get_users).to include("test@email.com" => "password")
     end
+    it("Does not allow an existing user to sign-up") do
+      chitter = Chitter.new
+      Chitter.sign_up("test@email.com", "password")
+      expect { Chitter.sign_up("test@email.com", "password") }.to raise_error("User already exists")
+    end
   end
 end
