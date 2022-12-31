e00b4085e8486cd3bd4172179f30e8e1f8d0b94b
Amir Khan <ak2022dev@gmail.com>
2022-12-31 13:46:17 +0000

    TDD add basic test for sign-up to be refactored

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,4 +1,11 @@
 # File: chitter_spec.rb
+require "chitter"
 
 RSpec.describe "chitter" do
+  context "sign-up" do
+    it("Allows a new user to sign-up") do
+      new_user = sign_up("test@email.com")
+      expect(new_user).to eq("test@email.com")
+    end
+  end
 end
