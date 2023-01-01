b50ffae5fd3e29f6466c38fd631d0dc4db50d5a6
Amir Khan <ak2022dev@gmail.com>
2023-01-01 15:33:51 +0000

    TDD refactor class variable users renamed to all_users in readiness to also record logged-in users

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,23 +1,23 @@
 class Chitter
 
   def initialize
-    @@users = {}
+    @@all_users = {}
   end
 
   def self.get_users
-    return @@users
+    return @@all_users
   end
 
   def self.sign_up(email, password)
     unless self.user_exists(email)
-      @@users[email] = password
+      @@all_users[email] = password
     else
       raise("User already exists")
     end
   end
 
   def self.user_exists(email)
-    return @@users.include?(email)
+    return @@all_users.include?(email)
   end
 
   def self.log_in(email, password)
@@ -25,7 +25,7 @@ class Chitter
   end
 
   def self.validate_password(email, password)
-    return @@users[email] == password
+    return @@all_users[email] == password
   end
 
 end
