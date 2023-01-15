3e9f5cb74c772a414d1d00d2428aa01a1a60a663
Amir Khan <ak2022dev@gmail.com>
2023-01-15 15:53:12 +0000

    Refactor add whitespace between methods for readability

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,19 +2,23 @@ require 'set'
 
 class Chitter
 
+
   def initialize
     @@all_users = {}
     @@logged_in_users = Set.new
     @@all_posts = Set.new
   end
 
+
   def self.all_posts
     return @@all_posts
   end
 
+
   def self.get_users
     return @@all_users
   end
+
 
   def self.sign_up(email, password)
     unless self.user_exists(email)
@@ -24,9 +28,11 @@ class Chitter
     end
   end
 
+
   def self.user_exists(email)
     return @@all_users.include?(email)
   end
+
 
   def self.log_in(email, password)
     if self.user_exists(email) && self.validate_password(email, password)
@@ -37,17 +43,21 @@ class Chitter
     end 
   end
 
+
   def self.validate_password(email, password)
     return @@all_users[email] == password
   end
+
 
   def self.register_login(email)
     @@logged_in_users.add(email)
   end
 
+
   def self.logged_in?(email)
     return @@logged_in_users.include?(email)
   end
+
 
   def self.log_out(email)
     if self.logged_in?(email)
@@ -57,8 +67,10 @@ class Chitter
     end
   end
 
+
   def self.post(email, message)
     @@all_posts.add(message)
   end
 
+  
 end
